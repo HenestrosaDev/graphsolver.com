9f0ab664dedf039cc94507980b99c1acedd8b3b4
José Carlos López <git@henestrosa.dev>
2026-01-24 23:06:11 +0100

    Add export image button to SectionVisualizer for PNG downloads

--- src/components/sections/SectionVisualizer.vue
+++ src/components/sections/SectionVisualizer.vue
@@ -369,6 +369,32 @@ watch([() => getGraphData(), highlightedPath], () => drawGraph(), {
 					</button>
 
 					<button
+						@click="exportImage"
+						class="p-2 rounded-full text-gray-500 hover:text-blue-600 hover:bg-blue-50 transition"
+						title="Descargar como imagen PNG"
+					>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							class="size-5"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke="currentColor"
+							stroke-width="2"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+							/>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
+							/>
+						</svg>
+					</button>
+
+					<button
 						@click="toggleFullscreen"
 						class="hidden lg:block p-2 rounded-full text-gray-500 hover:text-blue-600 hover:bg-blue-50 transition"
 						:class="{
