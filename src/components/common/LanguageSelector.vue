05e63384b8fb0326ef8be9fcf1e3eb173fcd1589
José Carlos López <git@henestrosa.dev>
2026-02-03 10:59:26 +0100

    Show the destination, not the status in `LanguageSelector`

--- src/components/common/LanguageSelector.vue
+++ src/components/common/LanguageSelector.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
+import { IconLanguage } from "@tabler/icons-vue";
 
 const { locale } = useI18n();
 
@@ -11,9 +12,9 @@ const toggleLanguage = () => {
 
 <template>
 	<button
-		class="flex items-center gap-2 rounded-lg border border-gray-200 bg-white px-3 py-1 text-sm font-bold text-gray-700 shadow-sm transition hover:bg-gray-50 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200 dark:hover:bg-slate-800"
+		class="flex items-center gap-1 rounded-lg border border-gray-200 bg-white px-3 py-1 text-sm font-bold text-gray-700 shadow-sm transition hover:bg-gray-50 dark:border-slate-700 dark:bg-slate-900 dark:text-slate-200 dark:hover:bg-slate-800"
 		@click="toggleLanguage"
 	>
-		<span>{{ locale === "es" ? $t("language.es") : $t("language.en") }}</span>
+		<span>{{ locale === "es" ? $t("language.en") : $t("language.es") }}</span>
 	</button>
 </template>
