51dbb444721f38c50079d08ed75a1ae8de2cd403
José Carlos López <git@henestrosa.dev>
2026-02-11 00:51:13 +0100

    Improve Spanish translations for weakly connected graphs

--- src/locales/es.ts
+++ src/locales/es.ts
@@ -217,8 +217,8 @@ export default {
 				forest: "Bosque",
 				connectedCyclic: "Cíclico conexo",
 				disconnectedCyclic: "Disconexo con ciclos",
-				weakConnectedCyclic: "Débilmente conexo con ciclos",
-				weakConnectedAcyclic: "Débilmente conexo acíclico",
+				weakConnectedCyclic: "Cíclico débilmente conexo",
+				weakConnectedAcyclic: "Acíclico débilmente conexo",
 				disconnected: "Disconexo",
 			},
 			hamiltonian: {
